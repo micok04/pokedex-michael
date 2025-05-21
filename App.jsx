5efa09c9621fd0f3c43cf570440ba9069b9cc846
micok04 <masm090929hmclnca8@lake.edu.mx>
2025-05-21 11:20:16 -0600

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,69 +1,84 @@
-import './App.css'
-import {useState, useEffect} from "react"
+import { useEffect, useState } from "react";
+import "./App.css";
+import Pokemon from "./components/Pokemon";
+
 function App() {
+  const [pokemones, setPokemones] = useState([]);
+  const [busquedaPokemon, setBusquedaPokemon] = useState("")
 
-
-
-  const [pokemones, setPokemones] = useState([])
-  const[busquedapokemon,setbusquedaPokemon] = useState("")
+ 
   useEffect(() => {
     const fetchPokemones = async () => {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
-      const data = await response.json()
-     const { results } = data
-    
-    const detallesPokemon = await Promise.all(
-    results.map(async (pokemon)=>{
-      const respuesta = await fetch(pokemon.url)
-      const datos = await respuesta.json()
-      return{
-      id:datos.id,
-      nombre:datos.name,
-      imagen:datos.sprites.front_default,
-      altura:datos.height,
-      peso:datos.weight
-      };
-    })   
-    )
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
+      const data = await response.json();
+      const { results } = data;
 
-setPokemones(detallesPokemon)
-  };
-    fetchPokemones()
+      const pokemonesDetalles = await Promise.all(
+        results.map(async (pokemon) => {
+          const response = await fetch(pokemon.url);
+          const poke = await response.json();
+          return {
+            id: poke.id,
+            name: poke.name,
+            image: poke.sprites.front_default,
+            height: poke.height,
+            weight: poke.weight,
+            types: poke.types.map((t) => t.type.name),
+            abilities: poke.abilities.map((a) => a.ability.name),
+          };
+        })
+      );
+
+      setPokemones(pokemonesDetalles);
+    };
+
+    fetchPokemones();
+  }, []);
+
+
+  const pokemonesFiltrados = pokemones.filter((p) =>{
+  return p.name.toLowerCase().includes(busquedaPokemon)
+  }
     
-  }, [])
- 
+  );
+
   return (
     <>
-         <h1 className='titulo'>Pokedex - mike bonito</h1>
-         <h2>Welcome to the Pokedex</h2>
-         <p>find your favorite pokemon!.</p>
-<input type="busqueda" "text"placeholder='busca a tu pokemon'/>
+      <h1 className="titulo">POKÉDEX</h1>
+      <h2>Welcome to the Pokedex</h2>
+      <p>Find your favorite Pokémon!</p>
 
-         {pokemones.map((pokemon) =>(
-          <div className="card" key={pokemon.id}>
-          <h1>{pokemon.nombre} #({pokemon.id}) </h1>
-<h2>welcome to the Pokedex</h2>
-<p>find your favorite pokemon!</p>
-<input
-className="busqueda"
-type="text"
-placeholder="busca a tu pokemon"
-value={busquedapokemon}
-/>
-          <img src={pokemon.imagen} alt=""/>
-          <p>altura : {pokemon.altura / 10} m</p>
-          <p>peso: {pokemon.peso / 10} kg</p>
-        </div>
-        ))}
-      
+      <input 
+      className="busqueda" 
+      type="text" 
+      placeholder="Busca a tu pokemon"
+      value={busquedaPokemon}
+      onChange={(e) => setBusquedaPokemon(e.target.value.toLowerCase()) }
+      />
+    
+     {pokemones.length === 0 ? (
+  <p className="loading">Loading...</p>
+) : (
+  <div className="container">
+    {pokemonesFiltrados.length > 0 ? (
 
-         <div>
-          <p>Developed by mike bonito</p>
-          <p>2025</p>
-          <p>All rights reserved</p>
-         </div>
+      pokemonesFiltrados.map((pokemon) => (
+          <Pokemon key={pokemon.id} datos={pokemon} />
+      ))
+    ) : (
+      <div className="no-results">No Pokémon found 😢</div>
+    )}
+  </div>
+)}
+
+
+      <div className="footer">
+        <p>Developed by Alberto Pimentel</p>
+        <p>2025</p>
+        <p>All rights reserved</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
